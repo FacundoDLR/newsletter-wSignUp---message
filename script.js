68daa14d26fa430e8ac4b35cf86f7498556aff29
Facundo DE LOS RIOS <facujofre96@gmail.com>
2024-12-01 21:54:02 +0100

    fix: fixes in desktop design and dynamic validations in js

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ const emailField = document.getElementById('email');
 const errorMessage = document.getElementById('error-message');
 const responseForm = document.getElementById('contact-form-response');
 const responseEmail = document.getElementById('email-response');
-const heroText = document.getElementById('hero-text')
-const heroImg = document.getElementById('hero-img')
+const main = document.getElementById('main');
 const resetButton = document.getElementById('reset-button');
 
 const handleSubmit = (e) => {
@@ -16,30 +15,28 @@ const handleSubmit = (e) => {
     if (!regex.test(emailValue)) {
         errorMessage.textContent = emailField.title;
         emailField.classList.add('contact-form', 'shake');
-        errorMessage.classList.add('shake')
+        errorMessage.classList.add('shake');
         setTimeout(function () {
             emailField.classList.remove("shake");
-            errorMessage.classList.remove('shake')
+            errorMessage.classList.remove('shake');
         }, 500);
         if (emailField.classList.contains('valid')) {
-            emailField.classList.replace('valid', 'invalid')
+            emailField.classList.replace('valid', 'invalid');
         } else {
-            emailField.classList.add('invalid')
+            emailField.classList.add('invalid');
         }
     } else {
         errorMessage.textContent = '';
 
-        emailField.classList.add('contact-form')
+        emailField.classList.add('contact-form');
         if (emailField.classList.contains('invalid')) {
-            emailField.classList.replace('invalid', 'valid')
+            emailField.classList.replace('invalid', 'valid');
         } else {
-            emailField.classList.add('valid')
-            alert('sending form')
+            emailField.classList.add('valid');
+            alert('sending form');
             setTimeout(() => {
-                heroImg.classList.add('none')
-                heroText.classList.add('none')
-                form.classList.add('none');
-                responseForm.classList.remove('none')
+                main.classList.add('none');
+                responseForm.classList.remove('none');
                 responseEmail.innerText = emailValue;
                 form.reset();
             }, 1000);
@@ -52,10 +49,8 @@ const handleSubmit = (e) => {
 form.addEventListener('submit', handleSubmit)
 
 const resetState = () => {
-    responseForm.classList.add('none')
-    heroImg.classList.remove('none')
-    heroText.classList.remove('none')
-    form.classList.remove('none');
-    emailField.classList.remove('valid')
+    responseForm.classList.add('none');
+    main.classList.remove('none');
+    emailField.classList.remove('valid');
 }
-resetButton.addEventListener('click', resetState)
+resetButton.addEventListener('click', resetState);
